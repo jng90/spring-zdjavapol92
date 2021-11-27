76e40aaf7cf33104e118c950a53d070e1c21bb83
jng90 <jng@o2.pl>
2021-11-27 12:25:22 +0100

    Putmapping + patchmapping

--- src/main/java/pl/sda/springzdjavapol92/controller/RestBookController.java
+++ src/main/java/pl/sda/springzdjavapol92/controller/RestBookController.java
@@ -3,11 +3,10 @@ package pl.sda.springzdjavapol92.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.springzdjavapol92.model.Book;
+
+import java.util.Optional;
 
 @RequestMapping("/api/v1")
 @RestController
@@ -17,6 +16,15 @@ public class RestBookController {
     public ResponseEntity<Book> addBook (@RequestBody Book book){
         book.setId(100);
           return ResponseEntity.status(HttpStatus.CREATED).body(book);
+    }
 
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> readBook(@PathVariable long id){
+        final Book book = Book.builder()
+                .title("Pan Tadeusz")
+                .author("Adam Mickiewicz")
+                .id(id)
+                .build();
+        return ResponseEntity.of(id < 20 ? Optional.of(book) : Optional.empty());
     }
 }
--- src/main/java/pl/sda/springzdjavapol92/controller/RestTodoController.java
+++ src/main/java/pl/sda/springzdjavapol92/controller/RestTodoController.java
@@ -1,5 +1,6 @@
 package pl.sda.springzdjavapol92.controller;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import pl.sda.springzdjavapol92.model.Todo;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 @RestController
 public class RestTodoController {
@@ -18,7 +20,7 @@ public class RestTodoController {
         return "test";
     }
 
-    @PostMapping("/api/v1/todos")
+    @PostMapping("/api/v1/todos/{id}")
     public ResponseEntity<Todo> addTodo(@RequestBody NewTodoDto dto){
         //TODO zapisac do bazy przeslanie zadanie
         final Todo todo = Todo.builder()
@@ -29,6 +31,35 @@ public class RestTodoController {
                 .created(Timestamp.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)))
                 .build();
         return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+    }
+
+    @GetMapping("/api/v1/todos/{id}")
+    public ResponseEntity<Todo> readTodo(@PathVariable long id) {
+        final Todo todo = Todo.builder()
+                .completed(false)
+                .id(id)
+                .title("Testowy")
+                .person("Felek")
+                .deadline("2021-12-12")
+                .build();
+        return ResponseEntity.of(id < 50 ? Optional.of(todo) : Optional.empty());
+    }
+
+    @PutMapping("/api/v1/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable long id, @RequestBody NewTodoDto todoUpdate){
+        //TODO wyciagamy obiekt o danym id i zmieniamy w nim pola na wartosci w todoUpdate
+        Todo todo = Todo.builder()
+                .id(id)
+                .person(todoUpdate.getPerson())
+                .deadline(todoUpdate.getDeadline())
+                .title(todoUpdate.getTitle())
+                .build();
+        return ResponseEntity.of(id < 10 ? Optional.of(todo) : Optional.empty());
+    }
+
+    @PatchMapping("/api/v1/todos/{id}")
+    public ResponseEntity<Todo> setTodoAsCompleted(@PathVariable long id){
+
     }
 
 
