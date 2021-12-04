d1113ca6315726b452533a2fd86732160f40d924
jng90 <jng@o2.pl>
2021-12-04 11:26:24 +0100

    Security config v1

--- src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.sda.springzdjavapol92.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,16 +11,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .httpBasic()
-                .realmName("spring")
-                .and()
                 .csrf()
                 .disable()
                 .headers()
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers("/todo/**").authenticated()
+                .antMatchers("/todo/**", "/person/**").authenticated()
                 .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
                 .and();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("1234").roles("USER", "ADMIN").and()
+                .withUser("ewa").password("abcd").roles("USER").and()
+                .withUser("admin").password("4321").roles("ADMIN");
     }
 }
