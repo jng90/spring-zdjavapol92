e86e49a93048322abdd495181aa5ac091863bc29
jng90 <jng@o2.pl>
2021-12-04 12:34:33 +0100

    Security config, antmatchers, encoder

--- src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
@@ -1,10 +1,15 @@
 package pl.sda.springzdjavapol92.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,7 +21,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers()
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers("/todo/**", "/person/**").authenticated()
+                //user ma dostep do /todo/list ale nie ma dostepu do todo/add
+                //admin ma dostep do /todo/**
+                .antMatchers("/todo/list").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/todo/**", "/person/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/api/v2/todos/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/api/v2/todos/**").hasRole("USER")
+                .antMatchers(HttpMethod.PATCH, "/api/v2/todos/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/v2/todos").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/v2/todos").permitAll()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
@@ -30,8 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("1234").roles("USER", "ADMIN").and()
+                .withUser("user").password("$2a$12$rZaMF9qvM5yw1mNIMeNbjeIiSLUL/p9Jf8jlFEUtGaqhS9zJWtOH.").roles("USER", "ADMIN").and()
                 .withUser("ewa").password("abcd").roles("USER").and()
                 .withUser("admin").password("4321").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
